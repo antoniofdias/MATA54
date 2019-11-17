076dcd40570c6705ccd5e80ad9274634e739e502
Tom <antoniofdiass@gmail.com>
2019-11-17 11:27:28 -0300

    Add Huffman

--- file-compression/main.cpp
+++ file-compression/main.cpp
@@ -5,13 +5,16 @@
 using namespace std;
 
 #define debug if (true) cout
+#define pb push_back
+
+map<char, string> dictionaryAB;
+map<char, string> dictionaryBA;
 
 struct node {
     bool terminal;
     unsigned int frequency;
     char content;
-    node *left_child = NULL;
-    node *right_child = NULL;
+    vector<node> children;
 
     bool operator <(const node & cmp_node) const {
         return frequency > cmp_node.frequency;
@@ -19,7 +22,37 @@ struct node {
 
 };
 
-int main () {
+void create_dictionary(node &node_visited, string code) {
+    if (!node_visited.terminal) {
+        create_dictionary(node_visited.children[1], code + '0');
+        create_dictionary(node_visited.children[0], code + '1');
+    }
+    else {
+        dictionary[node_visited.content] = code;
+    }
+}
+
+int main (int argc, char const *argv[]) {
+    if (argc == 3) {
+        switch(argv[1]) {
+            case "-c":
+                compress(argv[2]);
+                break;
+            case "-d":
+                decompress(argv[2]);
+                break;
+            default:
+                cerr << "Please make sure you want to compress (-c) or decompress (-d) a file.\n";
+                return 1;
+        }
+    }
+    else {
+        cerr << "Please check the number of parameters\n";
+        return 1;
+    }
+}
+
+void compress() {
     string str;
     node aux, root;
     map<char, int> table;
@@ -46,23 +79,25 @@ int main () {
             root = left;
         }
         else {
-            node right = huff.top();
+            node right = huff.top(), next;
             huff.pop();
-            aux.terminal = false;
-            aux.frequency = right.frequency + left.frequency;
-            aux.content = 64;
-            aux.right_child = &right;
-            aux.left_child = &left;
-            huff.push(aux);
+
+            next.terminal = false;
+            next.frequency = right.frequency + left.frequency;
+            next.content = 64;
+            next.children.pb(right);
+            next.children.pb(left);
+            huff.push(next);
         }
     }
 
-    bfs.push(root);
-    while (!bfs.empty()) {
-        aux = bfs.front();
-        bfs.pop();
-        cout << "Conteúdo: " << aux.content << ", Frequência: " << aux.frequency << endl;
-        //if (aux.left_child != NULL) bfs.push(*aux.left_child);
-        if (aux.right_child != NULL) bfs.push(*aux.right_child);
+    create_dictionary(root, "");
+    
+    for (auto kv : dictionary) {
+        cout << kv.first << ' ' << kv.second << endl;
     }
+}
+
+void decompress() {
+    
 }
